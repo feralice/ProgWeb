5dfec9081d5b3d5c732df9a1cefc36251cf52ff0
Fernanda Alice <113216913+feralice@users.noreply.github.com>
2023-08-31 20:32:21 -0400

    js6

--- js6/script.js
+++ js6/script.js
@@ -3,19 +3,17 @@ let divs = [];
 
 document.addEventListener("mousemove", function(e) {
     if (divs.length >= NUM_MAX) {
-        const divToRemove = divs.shift();
-        document.body.removeChild(divToRemove);
-        return;
+        return; 
     }
 
     const novaDiv = document.createElement("div");
     document.body.appendChild(novaDiv);
     divs.push(novaDiv);
-    novaDiv.style.left = (e.clientX - 20) + 'px';
-    novaDiv.style.top = (e.clientY - 20) + 'px';
+    novaDiv.style.left = (e.clientX - 40) + 'px';
+    novaDiv.style.top = (e.clientY - 40) + 'px';
 
     setTimeout(function() {
         const divToRemove = divs.shift();
         document.body.removeChild(divToRemove);
-    }, 100);
+    }, 50);
 });
