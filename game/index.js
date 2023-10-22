f72ca062c6d6a7900b803cfb63d625601233c91d
Fernanda Alice <113216913+feralice@users.noreply.github.com>
2023-10-22 17:45:48 -0400

    tp2 - game1

--- game/index.js
+++ game/index.js
@@ -1,13 +1,28 @@
 import express from 'express';
 import router from './src/routes/routes';
 import { engine } from 'express-handlebars';
+import sass from "node-sass-middleware"
 
 const morgan = require('morgan');
 const app = express();
 const PORT = 3000;
 
+app.use(sass({
+  src: `${__dirname}/public/scss`,
+  dest: `${__dirname}/public/css`,
+  outputStyle: "compressed",
+  prefix: "/css",
+}));
+
+//pra enviar arquivos estaticos tipo fotos
+app.use("/img",express.static(`${__dirname}/public/img`));
+
 //cria a engine
-app.engine('handlebars', engine());
+app.engine('handlebars', engine({
+  //helpers: require(`${__dirname}/src/views/helpers/helpers`)
+  layoutsDir: `${__dirname}/src/views/layouts`,
+  defaultLayout: 'main',
+}));
 
 //seta a engine
 app.set('view engine', 'handlebars');
@@ -18,5 +33,5 @@ app.use(router);
 app.use(morgan('combined'));
 
 app.listen(PORT, () => {
-  console.log(`Servidor rodando em "http://localhost:${PORT}" :D`);
+  console.log(`Servidor rodando em "http://localhost:${PORT}" : D`);
 });
--- game/src/controllers/main.js
+++ game/src/controllers/main.js
@@ -1,9 +1,15 @@
 const index = (req, res) => {
-    res.send('Hello world');
+    res.render("main/index", {
+        msg: "ola, bem vindo",
+    });
 }
 
 const about = (req, res) => {
-    res.send('Pagina about');
+    res.render("main/about", {
+        title: "O jogo do dino chrome",
+        paragraph: "T-Rex Game, também denominado Chrome Dino é um jogo integrado ao navegador Google Chrome. O jogo foi criado por Edward Jung, Sebastien Gabriel e Alan Bettes em 2014 como um ovo de páscoa que aparece quando estiver offline e fizer uma pesquisa no Google Chrome.",
+        image: "img/dinoPhoto.jpg",
+    });
 }
 
-export default {index, about};
+export default { index, about };
