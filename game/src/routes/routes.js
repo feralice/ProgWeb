374e6cd906297b21279b5ecb619c5ae1af406cec
Fernanda Alice <113216913+feralice@users.noreply.github.com>
2023-10-23 14:54:52 -0400

    tp2 - game6

--- game/src/routes/routes.js
+++ game/src/routes/routes.js
@@ -1,9 +1,10 @@
 //arquivo de rotas
 import express from 'express';
 import main from '../controllers/main';
+import areaController from '../controllers/area'
 const router = express.Router();
 
-//rota para o /
+//rota para o home
 router.get('/', main.index);
 
 //rota para o /about
@@ -14,5 +15,7 @@ router.get('/ui', main.ui);
 
 //rota para o jogo
 router.get('/game', main.game);
+
+router.get('/areas', areaController.index);
 
 export default router;
